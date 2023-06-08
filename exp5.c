ec75b8d42efe0928c4b3f132e7b2bdcb9979470c
Shanmukh <91821966+randomemain@users.noreply.github.com>
2023-06-08 22:49:32 +0530

    Update exp5.c

--- exp5.c
+++ exp5.c
@@ -2,41 +2,53 @@
 
 #define MAX_NODES 10
 
-int a[MAX_NODES][MAX_NODES], n;
-
-void adj(int k)
+void findAdjacentNodes(int root, int adjacencyMatrix[MAX_NODES][MAX_NODES], int n)
 {
     int i, j;
     printf("Adjacent nodes of the root node:\n");
-    printf("%d\n", k);
-    for (j = 1; j <= n; j++)
-        if (a[k][j] == 1 || a[j][k] == 1)
-            printf("%d\t", j);
-    printf("\n");
-    for (i = 1; i <= n; i++)
+    printf("%d\n", root);
+
+    for (j = 0; j < n; j++)
     {
-        if (a[k][i] == 0 && a[i][k] == 0 && i != k)
+        if (adjacencyMatrix[root][j] == 1 || adjacencyMatrix[j][root] == 1)
+        {
+            printf("%d\t", j);
+        }
+    }
+    printf("\n");
+
+    for (i = 0; i < n; i++)
+    {
+        if (i != root && adjacencyMatrix[root][i] == 0 && adjacencyMatrix[i][root] == 0)
+        {
             printf("%d\t", i);
+        }
     }
     printf("\n");
 }
 
 int main()
 {
-    int i, j, root;
+    int adjacencyMatrix[MAX_NODES][MAX_NODES];
+    int n, i, j, root;
+
     printf("Enter the number of nodes: ");
     scanf("%d", &n);
+
     printf("Enter the adjacency matrix:\n");
-    for (i = 1; i <= n; i++)
+    for (i = 0; i < n; i++)
     {
-        for (j = 1; j <= n; j++)
+        for (j = 0; j < n; j++)
         {
             printf("Enter the connection from %d to %d: ", i, j);
-            scanf("%d", &a[i][j]);
+            scanf("%d", &adjacencyMatrix[i][j]);
         }
     }
+
     printf("Enter the root node: ");
     scanf("%d", &root);
-    adj(root);
+
+    findAdjacentNodes(root, adjacencyMatrix, n);
+
     return 0;
 }
